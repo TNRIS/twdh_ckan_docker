2aaa744a22db747c5ef290ed77fd5c1a04d01038
Ben Bright <ben.bright@twdb.texas.gov>
2024-12-05 13:09:46 -0600

    Renamed and improved 01_ckan_db_upgrade.sh
    Renamed 01_ckan_upgrade.sh to 01_ckan_db_upgrade.sh
    
    Introduced conditional statement so that we can keep this file in place
    but only have it run when we set UPGRADE=1

---
+++ docker/ckan/entrypoint.d/01_ckan_db_upgrade.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "@@@@@@ UPGRADE CKAN DB @@@@@@"
+
+UPGRADE=1
+
+if [ "$UPGRADE" -ne 1 ]; then
+    echo "Warning: UPGRADE not enabled. If you want db upgrade to run, set UPGRADE=1 in twdh_ckan_docker/docker/ckan/entrypoint.d/01_ckan_db_upgrade.sh"
+      exit
+else 
+  ckan -c /srv/app/production.ini db upgrade 
+fi
+
--- docker/ckan/entrypoint.d/01_ckan_upgrade.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "@@@@@@ UPGRADE CKAN DB @@@@@@"
-ckan -c /srv/app/production.ini db upgrade 
