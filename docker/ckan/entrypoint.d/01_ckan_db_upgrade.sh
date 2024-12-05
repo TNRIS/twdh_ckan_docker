266dab76fcfbc9189a1f645651f8f81ffac7a4f8
Ben Bright <ben.bright@twdb.texas.gov>
2024-12-05 13:55:53 -0600

    Removed errant exit statement

--- docker/ckan/entrypoint.d/01_ckan_db_upgrade.sh
+++ docker/ckan/entrypoint.d/01_ckan_db_upgrade.sh
@@ -5,7 +5,6 @@ UPGRADE=0
 
 if [ "$UPGRADE" -ne 1 ]; then
     echo "Warning: UPGRADE not enabled. If you want db upgrade to run, set UPGRADE=1 in twdh_ckan_docker/docker/ckan/entrypoint.d/01_ckan_db_upgrade.sh"
-      exit
 else 
   ckan -c /srv/app/production.ini db upgrade 
 fi
