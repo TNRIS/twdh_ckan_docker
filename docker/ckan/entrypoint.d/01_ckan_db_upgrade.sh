53e1f8cacf725801447fe0ab6d30725a2c37dadb
Ben Bright <ben.bright@twdb.texas.gov>
2024-12-06 08:57:08 -0600

    Set UPGRADE=0

--- docker/ckan/entrypoint.d/01_ckan_db_upgrade.sh
+++ docker/ckan/entrypoint.d/01_ckan_db_upgrade.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "@@@@@@ UPGRADE CKAN DB @@@@@@"
 
-UPGRADE=1
+UPGRADE=0
 
 if [ "$UPGRADE" -ne 1 ]; then
     echo "Warning: UPGRADE not enabled. If you want db upgrade to run, set UPGRADE=1 in twdh_ckan_docker/docker/ckan/entrypoint.d/01_ckan_db_upgrade.sh"
