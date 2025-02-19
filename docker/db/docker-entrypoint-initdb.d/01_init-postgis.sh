7112ad8c80603aea60ccec08f934d916343d3a26
Ben Bright <ben.bright@twdb.texas.gov>
2025-02-19 13:36:42 -0600

    Reenabled postgix setup

--- docker/db/docker-entrypoint-initdb.d/01_init-postgis.sh
+++ docker/db/docker-entrypoint-initdb.d/01_init-postgis.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
-#set -e
+!/bin/sh
+set -e
 
-#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "ckan" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "ckan" <<-EOSQL
     #CREATE EXTENSION postgis;
-#EOSQL
+EOSQL
