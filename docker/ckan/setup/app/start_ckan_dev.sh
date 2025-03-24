44d3fd1b3dc38a42085d7b9db0a75b6f25c55c83
Ben Bright <ben.bright@twdb.texas.gov>
2025-03-24 12:36:07 -0500

    Fixed support for nockan mode

--- docker/ckan/setup/app/start_ckan_dev.sh
+++ docker/ckan/setup/app/start_ckan_dev.sh
@@ -18,23 +18,26 @@ fi
 # Set the common uwsgi options
 UWSGI_OPTS="--socket /tmp/uwsgi.sock --pidfile=/tmp/uwsgi.pid --uid ckan --gid ckan --http :5000 --master --enable-threads --wsgi-file /srv/app/wsgi.py --module wsgi:application --lazy-apps --gevent 2000 -p 2 -L --gevent-early-monkey-patch --vacuum --harakiri 50 --callable application --single-interpreter --need-app --disable-logging --log-4xx --log-5xx --log-slow 5000"
 
-# Run the prerun script to init CKAN and create the default admin user
-python prerun.py || { echo '[CKAN prerun] FAILED. Exiting...' ; exit 1; }
+if [ "$TWDH_DEV_MODE" != nockan ];
+then 
+  # Run the prerun script to init CKAN and create the default admin user
+  python prerun.py || { echo '[CKAN prerun] FAILED. Exiting...' ; exit 1; }
 
-# Check if we are in maintenance mode and if yes serve the maintenance pages
-if [ "$MAINTENANCE_MODE" = true ]; then PYTHONUNBUFFERED=1 python maintenance/serve.py; fi
+  # Check if we are in maintenance mode and if yes serve the maintenance pages
+  if [ "$MAINTENANCE_MODE" = true ]; then PYTHONUNBUFFERED=1 python maintenance/serve.py; fi
 
-# Run any after prerun/init scripts provided by images extending this one
-if [[ -d "${APP_DIR}/docker-afterinit.d" ]]
-then
-    for f in ${APP_DIR}/docker-afterinit.d/*; do
-        case "$f" in
-            *.sh)     echo "$0: Running after prerun init file $f"; . "$f" ;;
-            *.py)     echo "$0: Running after prerun init file $f"; python "$f"; echo ;;
-            *)        echo "$0: Ignoring $f (not an sh or py file)" ;;
-        esac
-        echo
-    done
+  # Run any after prerun/init scripts provided by images extending this one
+  if [[ -d "${APP_DIR}/docker-afterinit.d" ]]
+  then
+      for f in ${APP_DIR}/docker-afterinit.d/*; do
+          case "$f" in
+              *.sh)     echo "$0: Running after prerun init file $f"; . "$f" ;;
+              *.py)     echo "$0: Running after prerun init file $f"; python "$f"; echo ;;
+              *)        echo "$0: Ignoring $f (not an sh or py file)" ;;
+          esac
+          echo
+      done
+  fi
 fi
 
 # Start supervisord
