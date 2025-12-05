7c1abcfd145d5db3d88ac325444816db90d9333e
Ben Bright <ben.bright@twdb.texas.gov>
2025-12-05 14:11:50 -0600

    Added upgrade toggle to afterinit migration scripts

--- docker/ckan/afterinit.d/01_security.sh
+++ docker/ckan/afterinit.d/01_security.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 echo "@@@@@@ INIT SECURITY @@@@@@"
-ckan -c /srv/app/production.ini security migrate
+
+UPGRADE=0
+
+if [ "$UPGRADE" -ne 1 ]; then
+    echo "Warning: CKAN INIT SECURITY not enabled. If you want ckanext-security upgrade to run, set UPGRADE=1 in twdh_ckan_docker/docker/ckan/entrypoint.d/01_security.sh"
+else 
+  ckan -c /srv/app/production.ini security migrate
+fi
