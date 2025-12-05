c4b0d12bcd2467d6208892283179e3879b3da40e
Ben Bright <ben.bright@twdb.texas.gov>
2025-12-05 14:11:50 -0600

    Added upgrade toggle to afterinit migration scripts

--- docker/ckan/afterinit.d/05_pages.sh
+++ docker/ckan/afterinit.d/05_pages.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 echo "@@@@@@ INIT CKANEXT-PAGES @@@@@@"
-ckan -c /srv/app/production.ini db upgrade -p pages
+
+UPGRADE=0
+
+if [ "$UPGRADE" -ne 1 ]; then
+    echo "Warning: CKAN INIT CKANEXT-PAGES not enabled. If you want ckanext-pages upgrade to run, set UPGRADE=1 in twdh_ckan_docker/docker/ckan/entrypoint.d/01_pages.sh"
+else 
+  ckan -c /srv/app/production.ini db upgrade -p pages
+fi
