87ccb841446715f69b22fcfb1c261a9fcb2480b7
Ben Bright <ben.bright@twdb.texas.gov>
2025-12-15 14:43:49 -0600

    Turned off upgrade migration scripts

--- docker/ckan/afterinit.d/03_checklink.sh
+++ docker/ckan/afterinit.d/03_checklink.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "@@@@@@ INIT CKANEXT-CHECK_LINK @@@@@@"
 
-UPGRADE=1
+UPGRADE=0
 
 if [ "$UPGRADE" -ne 1 ]; then
     echo "Warning: CKAN INIT CKANEXT-CHECK_LINK not enabled. If you want ckanext-checklink upgrade to run, set UPGRADE=1 in twdh_ckan_docker/docker/ckan/entrypoint.d/01_checklink.sh"
