286050d26fb34fa16709847701ed83071b3f66cc
Ben Bright <ben.bright@twdb.texas.gov>
2026-08-10 15:09:49 -0500

    Asset permissions fix attempt

--- docker/ckan/afterinit.d/06_assets.sh
+++ docker/ckan/afterinit.d/06_assets.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 echo "@@@@@@ BUILT ASSETS @@@@@@"
 
-ckan -c /srv/app/production.ini asset build && chown -R ckan.ckan /srv/app/data/webassets
+ckan -c /srv/app/production.ini asset build 
+
+echo "@@@@@@ SET ASSET PERMISSIONS @@@@@@"
+chown -R ckan.ckan /srv/app/data/webassets
