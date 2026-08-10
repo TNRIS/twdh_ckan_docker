e3e4418e45e5328e8ebdc3f19338dea175015a88
Ben Bright <ben.bright@twdb.texas.gov>
2026-08-10 13:53:51 -0500

    Added asset build script to afterinit.d

--- docker/ckan/afterinit.d/06_assets.sh
+++ docker/ckan/afterinit.d/06_assets.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "@@@@@@ BUILT ASSETS @@@@@@"
 
-ckan -c /srv/app/production.ini asset build
+ckan -c /srv/app/production.ini asset build && chown -R ckan.ckan /srv/app/data/webassets
