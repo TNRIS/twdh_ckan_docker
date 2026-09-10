b69cf7e7205f8e090ee69e128838f43e6c737e25
Ben Bright <ben.bright@twdb.texas.gov>
2026-09-10 11:40:25 -0500

    Temporarily turn off init scripts for ECS debugging

--- docker/ckan/setup/app/prerun.py
+++ docker/ckan/setup/app/prerun.py
@@ -214,7 +214,8 @@ if __name__ == '__main__':
     else:
         check_solr_connection()
         check_db_connection()
-        init_db()
-        if os.environ.get('CKAN_DATASTORE_WRITE_URL'):
-            init_datastore()
-        create_sysadmin()
+        # TESTING - these need to be turned back on!
+        #init_db()
+        #if os.environ.get('CKAN_DATASTORE_WRITE_URL'):
+            #init_datastore()
+        #create_sysadmin()
