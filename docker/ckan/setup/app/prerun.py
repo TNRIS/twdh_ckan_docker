8e64281cf895d85af19649645127a41c5b7f485d
Ben Bright <ben.bright@twdb.texas.gov>
2026-09-22 15:57:02 -0500

    Turned off init scripts

--- docker/ckan/setup/app/prerun.py
+++ docker/ckan/setup/app/prerun.py
@@ -214,7 +214,7 @@ if __name__ == '__main__':
     else:
         check_solr_connection()
         check_db_connection()
-        init_db()
-        if os.environ.get('CKAN_DATASTORE_WRITE_URL'):
-            init_datastore()
-        create_sysadmin()
+        #init_db()
+        #if os.environ.get('CKAN_DATASTORE_WRITE_URL'):
+        #   init_datastore()
+        #create_sysadmin()
